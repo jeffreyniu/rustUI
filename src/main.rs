77ab106d8e3b3350ed3482237d2a5e440a331066
jeffreyniu <niutianbo@hotmail.com>
2026-01-23 19:49:20 +0800

    switch to gpui

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-use eframe::egui;
 
 mod main_window;
 
-fn main() -> Result<(), eframe::Error> {
-    let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default().with_inner_size([600.0, 400.0]),
-        ..Default::default()
-    };
-    eframe::run_native(
-        "Tools",
-        options,
-         Box::new(|cc| Ok(Box::new(main_window::MainWindow::new(cc))))
-    )
+fn main() {
+
 }

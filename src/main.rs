8253e76b2196e23452f33bb82092e038d1ad4901
jefniu-debian <niutianbo@hotmail.com>
2026-01-24 15:03:08 +0800

    init window

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use gpui::{
+    prelude::*, px, size, App, Application, Bounds, WindowBounds, WindowOptions
+};
 
 mod main_window;
 
 fn main() {
-
+	Application::new().run(|cx: &mut App| {
+        let bounds = Bounds::centered(None, size(px(500.), px(500.0)), cx);
+        cx.open_window(
+            WindowOptions {
+                window_bounds: Some(WindowBounds::Windowed(bounds)),
+                ..Default::default()
+            },
+            |_, cx| {
+                cx.new(|_| main_window::MainWindow::new())
+            },
+        )
+        .unwrap();
+    });
 }
--- src/main_window.rs
+++ src/main_window.rs
@@ -1,3 +1,8 @@
+use gpui::{
+    div, prelude::*, px, rgb, size, App, Application, Bounds, Context, SharedString, Window,
+    WindowBounds, WindowOptions,
+};
+
 #[path ="./fonts_cus.rs"]
 mod fonts_cus;
 
@@ -5,7 +10,7 @@ mod fonts_cus;
 mod pdf_cus;
 
 pub struct MainWindow {
-    pdf_file_path: String,
+    pub pdf_file_path: String,
 }
 
 impl Default for MainWindow {
@@ -17,6 +22,12 @@ impl Default for MainWindow {
 }
 
 impl MainWindow {
+
+    // 构造方法（接收创建上下文）
+    pub fn new() -> Self {
+        Self::default() // 返回默认实例
+    }
+    
     fn read_pdf(&self, _path: &str) {
         let result = pdf_cus::load_pdf_of_hands_language(_path);
 
@@ -30,5 +41,35 @@ impl MainWindow {
                 println!("Error reading PDF or saving images: {:?}", e);
             }
         }
+    }
+}
+
+impl Render for MainWindow {
+     fn render(&mut self, _window: &mut Window, _cx: &mut Context<Self>) -> impl IntoElement {
+        div()
+            .flex()
+            .flex_col()
+            .gap_3()
+            .bg(rgb(0x505050))
+            .size(px(500.0))
+            .justify_center()
+            .items_center()
+            .shadow_lg()
+            .border_1()
+            .border_color(rgb(0x0000ff))
+            .text_xl()
+            .text_color(rgb(0xffffff))
+            .child(format!("Hello, {}!", &self.pdf_file_path))
+            .child(
+                div()
+                    .flex()
+                    .gap_2()
+                    .child(div().size_8().bg(gpui::red()))
+                    .child(div().size_8().bg(gpui::green()))
+                    .child(div().size_8().bg(gpui::blue()))
+                    .child(div().size_8().bg(gpui::yellow()))
+                    .child(div().size_8().bg(gpui::black()))
+                    .child(div().size_8().bg(gpui::white())),
+            )
     }
 }
